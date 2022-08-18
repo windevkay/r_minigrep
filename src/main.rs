daadec9a1dee30629251be3e2b2122393d5c8ad8
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-08-17 20:15:47 -0400

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::env;
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
-    println!("seraching for {} in {}", query, file_path);
+    println!(
+        "searching for '{}' in file: {}",
+        config.query, config.file_path
+    );
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
 }
