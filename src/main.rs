63957303a7e75a4ce7eefff1cf237dc2c5d1ebd9
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-08-21 14:35:31 -0400

    all done

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
-    println!(
-        "searching for '{}' in file: {}",
-        config.query, config.file_path
-    );
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
         process::exit(1);
     }
 }
